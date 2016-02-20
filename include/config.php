3663db0650f46c092b73498d083cd4407346f4b1
Manraj Singh <manrajsinghgrover@gmail.com>
2016-02-20 22:00:01 +0530

    config.php modified with private settings

--- include/config.php
+++ include/config.php
@@ -1,3 +1,9 @@
 <?php
-    
+    $private = file_get_contents("../private/settings.json");
+    $json = json_decode($private, true);
+    define('DB_USERNAME', $json['username']);
+    define('DB_PASSWORD', $json['password']);
+    define('DB_HOST', $json['db_host']);
+    define('DB_NAME', $json['db_name']);
+    define("GCM_API", $json['gcm_api']);
 ?>
